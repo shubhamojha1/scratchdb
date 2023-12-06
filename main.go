ee9d18af70efcfc47829eaaf032d16deccf285a5
Shubham <subham.k.ojha@gmail.com>
2023-12-07 01:32:13 +0530

    savedata2() done. tmp and original file access issue

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
+	"time"
 )
 
 func main() {
@@ -13,7 +15,7 @@ func main() {
 	data := []byte(str)
 	filepath := "./sample.txt"
 
-	err := SaveData(filepath, data)
+	err := SaveData2(filepath, data)
 	if err != nil {
 		fmt.Println("Error: ", err)
 	} else {
@@ -31,4 +33,30 @@ func SaveData(path string, data []byte) error {
 
 	_, err = fp.Write(data)
 	return err
+}
+
+func SaveData2(path string, data []byte) error {
+	rand.Seed(time.Now().UnixNano())
+
+	// randomInt := rand.Intn(100-1+1) + 1
+	tmp := fmt.Sprintf("%s.tmp.%d", path, 1)
+	fp, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+
+	_, err = fp.Write(data)
+	if err != nil {
+		os.Remove(tmp)
+		return err
+	}
+	// fp.Close()
+	// return os.Rename(tmp, path)
+	err = os.Rename(tmp, path)
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
