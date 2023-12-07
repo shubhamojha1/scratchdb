b5c81a8b197d70a439c37202156bfaf1d4c14b10
Shubham <subham.k.ojha@gmail.com>
2023-12-08 01:14:38 +0530

    added SaveData3() using fsync syscall & functions for append-only logs

--- main.go
+++ main.go
@@ -9,13 +9,14 @@ import (
 
 func main() {
 	// SaveData("./sample.txt", [])
+	fmt.Println("Type something...")
 	var str string
 	fmt.Scanln(&str)
 
 	data := []byte(str)
 	filepath := "./sample.txt"
 
-	err := SaveData2(filepath, data)
+	err := SaveData3(filepath, data)
 	if err != nil {
 		fmt.Println("Error: ", err)
 	} else {
@@ -38,25 +39,58 @@ func SaveData(path string, data []byte) error {
 func SaveData2(path string, data []byte) error {
 	rand.Seed(time.Now().UnixNano())
 
-	// randomInt := rand.Intn(100-1+1) + 1
-	tmp := fmt.Sprintf("%s.tmp.%d", path, 1)
+	randomInt := rand.Intn(100-1+1) + 1
+	tmp := fmt.Sprintf("%s.tmp.%d", path, randomInt)
 	fp, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
 	if err != nil {
 		return err
 	}
-	defer fp.Close()
 
 	_, err = fp.Write(data)
 	if err != nil {
 		os.Remove(tmp)
 		return err
 	}
-	// fp.Close()
-	// return os.Rename(tmp, path)
-	err = os.Rename(tmp, path)
+	fp.Close() // defer will not work here, need to close file before renaming it.
+	return os.Rename(tmp, path)
+}
+
+func SaveData3(path string, data []byte) error {
+	rand.Seed(time.Now().UnixNano())
+
+	randomInt := rand.Intn(100-1+1) + 1
+	tmp := fmt.Sprintf("%s.tmp.%d", path, randomInt)
+	fp, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
 	if err != nil {
 		return err
 	}
-	return nil
 
+	_, err = fp.Write(data)
+	if err != nil {
+		os.Remove(tmp)
+		return err
+	}
+
+	err = fp.Sync() // linux fsync {flushes data to the disk before renaming it} https://youtu.be/JK2ZIx8jRu4?si=TqsYujUVBrFxKJVB
+	if err != nil {
+		os.Remove(tmp)
+		return err
+	}
+	fp.Close()
+	return os.Rename(tmp, path)
+
+}
+
+func LogCreate(path string) (*os.File, error) {
+	return os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0664)
+}
+
+func LogAppend(fp *os.File, line string) error {
+	buf := []byte(line)
+	buf = append(buf, '\n')
+	_, err := fp.Write(buf)
+	if err != nil {
+		return err
+	}
+	return fp.Sync()
 }
