954864d84c762a99ecf473d21067d0d1c3a33d77
Shubham <subham.k.ojha@gmail.com>
2023-12-16 11:32:37 +0530

    feat(btree): Introduce function to get node size (in bytes)

--- b_tree.go
+++ b_tree.go
@@ -117,3 +117,8 @@ func (node BNode) getVal(idx uint16, t *testing.T) []byte {
 	vlen := binary.LittleEndian.Uint16(node.data[pos+2:])
 	return node.data[pos+4+klen:][:vlen]
 }
+
+// node size in bytes
+func (node BNode) nbytes() uint16 {
+	return node.kvPos(node.nkeys(), nil) // (?)
+}
